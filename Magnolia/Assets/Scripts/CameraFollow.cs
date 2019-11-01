cb23538fd040d652ecdd6c89faab0699f086baef
EmilioSantillana <emiprec77@gmail.com>
2019-11-01 14:11:26 -0600

    Basic movements
    Basics movements added

---
+++ Magnolia/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    public Transform target;
+    public float smoothSpeed = 5f;
+    public Vector3 offset;
+
+    private void Awake()
+    {
+        offset = new Vector3(15, 3.5f, 0);
+        transform.position = target.position + offset;
+    }
+    private void Start()
+    {
+        playerMovement = target.GetComponent<PlayerMovement>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.fixedDeltaTime);
+        transform.position = smoothedPosition;
+
+        if (playerMovement.IsGrounded == false)
+        {
+            offset = new Vector3(20, 1, 0);
+        }
+        else
+        {
+            offset = new Vector3(15, 3.5f, 0);
+        }
+    }
+}
---
+++ Magnolia/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody m_Rigidbody;
+    private Vector3 gravity;
+    private int jumps;
+
+    public bool IsGrounded { get; private set; }
+
+    public bool doubleJump;
+    public float gravityForce = -9.81f;
+    public float movementSpeed = 10;
+    public float jumpForce = 7;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        gravity = new Vector3(0, gravityForce, 0);
+        IsGrounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        m_Rigidbody.AddForce(gravity, ForceMode.Acceleration);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            transform.position += transform.forward * movementSpeed * Time.fixedDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = new Quaternion(0, 90, 0, 0);
+            transform.position += transform.forward * movementSpeed * Time.fixedDeltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        
+        if (IsGrounded)
+        {
+            jumps = 0;
+        }
+        if (IsGrounded || (doubleJump && jumps < 2))
+        {
+            m_Rigidbody.velocity = Vector3.up * jumpForce * Time.fixedDeltaTime;
+            jumps += 1;
+            IsGrounded = false;
+        }      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            IsGrounded = true;
+        }
+    }
+}
