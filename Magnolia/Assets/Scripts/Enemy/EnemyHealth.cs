c6b110fafa34a15edd48253737e7da58e79f30f5
EmilioSantillana <emiprec77@gmail.com>
2019-11-08 13:02:54 -0600

    Basic drop money
    Enemies drop money

--- Magnolia/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Magnolia/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public int health = 100;
+    public GameObject[] drops;
 
     private void Update()
     {
         if (health <= 0)
         {
+            GameObject.Instantiate(drops[Random.Range(0,drops.Length)], transform.position, new Quaternion());
             Destroy(this.gameObject);
         }
     }
---
+++ Magnolia/Assets/Scripts/Money.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
