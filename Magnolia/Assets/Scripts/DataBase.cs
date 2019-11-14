1a8753c31b214d53a25ba6204998a31f9bacea6e
EmilioSantillana <emiprec77@gmail.com>
2019-11-13 19:26:48 -0600

    DataBase
    DataBase Dinero

---
+++ Magnolia/Assets/Scripts/DataBase.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class DataBase
+{
+    private static string conn = "URI=file:" + Application.dataPath + "/DataBase/Magnolia.s3db";
+
+    public static int GetDinero(int id)
+    {
+        int dinero=0;
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT dinero FROM Dinero WHERE id = " +id;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            dinero = reader.GetInt32(0);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+        return dinero;
+    }
+
+    public static void SetDinero(int id, int dinero)
+    {
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "UPDATE Dinero SET dinero = " +dinero +" WHERE id = " + id;
+        dbcmd.CommandText = sqlQuery;
+        if (dbcmd.ExecuteNonQuery() > 0)
+        {
+            Debug.Log("Se modifico, dinero = " + dinero);
+        }
+        else
+        {
+            Debug.Log("No se modifico");
+        }
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }   
+}
--- Magnolia/Assets/Scripts/Money.cs
+++ Magnolia/Assets/Scripts/Money.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Money : MonoBehaviour
 {
+    public int valor;
+    private int valorX;
+
+    private void Start()
+    {
+        valorX = valor; 
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log(valorX);
+            int dbMoney = DataBase.GetDinero(1);
+            dbMoney += valorX;
+            DataBase.SetDinero(1, dbMoney);
             GameObject.Destroy(this.gameObject);
         }
     }
