b90a10388258aef1344084adee9d893e1b3c042b
EmilioSantillana <emiprec77@gmail.com>
2020-05-18 13:08:43 -0500

    Erick Added
    Erick was added with animations and scripts

---
+++ Elemental Legend/Assets/Scripts/Guns/Escopeta/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float normalization;
+    private Vector3 normalizedOrientation;
+    private Rigidbody rb;
+
+    public Vector3 direction;
+    public float speed;
+    public int damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        normalization = Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.y, 2));
+        normalizedOrientation = new Vector3(direction.x / normalization, direction.y / normalization);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(transform.forward.x, transform.forward.y * normalizedOrientation.x, transform.forward.z) * speed;
+        StartCoroutine(DestroyBullet());
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("hit enemy");
+            other.GetComponent<EnemyHealth>().health -= damage;
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("Piso"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Elemental Legend/Assets/Scripts/Player/PlayerMovement.cs
+++ Elemental Legend/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {  
     private Rigidbody m_Rigidbody;
     private Vector3 gravity;
+    private CameraFollow cameraFollow;
+    private Gun gun;
+    private int cont = 0;
     private int jumps;
 
     public bool IsGrounded { get; private set; }
@@ -15,9 +18,11 @@ public class PlayerMovement : MonoBehaviour
     public float gravityForce = -9.81f;
     public float movementSpeed = 10;
     public float jumpForce = 15;
+    public Transform mouse;
 
     void Start()
     {
+        cameraFollow = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();      
         m_Rigidbody = GetComponent<Rigidbody>();
         gravity = new Vector3(0, gravityForce, 0);
         IsGrounded = true;
@@ -25,23 +30,69 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (gun == null)
+        {
+            gun = GetComponentInChildren<Gun>();
+        }
+
+        mouse.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraFollow.generalOffset.x));
+
         m_Rigidbody.AddForce(gravity, ForceMode.Acceleration);
 
         if (Input.GetKey(KeyCode.D))
         {
             turned = false;
-            //transform.localScale = new Vector3(1, 1, 1);
             transform.position += transform.forward * movementSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
             turned = true;
-            //transform.localScale = new Vector3(-1, 1, -1);
             transform.position += -transform.forward * movementSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
         {
             Jump();
+        }
+
+        LookMouse(); 
+    }
+
+    private void LookMouse()
+    {
+        if (turned == false && mouse.localPosition.z < 0)
+        {
+            if (cont == 0)
+            {
+                transform.localScale = new Vector3(-1, 1, -1);
+                gun.transform.localScale = new Vector3(1f, 1f, -1f);
+                gun.roty = 180;
+                cont = 1;
+            }
+            else if (cont == 1)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                gun.transform.localScale = new Vector3(1f, 1f, 1f);
+                gun.roty = 0;
+                cont = 0;
+            }
+
+        }
+        else if (turned == true && mouse.localPosition.z < 0)
+        {
+            if (cont == 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                gun.transform.localScale = new Vector3(1f, 1f, 1f);
+                gun.roty = 0;
+                cont = 1;
+            }
+            else if (cont == 1)
+            {
+                transform.localScale = new Vector3(-1, 1, -1);
+                gun.transform.localScale = new Vector3(1f, 1f, -1f);
+                gun.roty = 180;
+                cont = 0;
+            }
         }
     }
 
