b3653b2a6f482b05fba525b842d6001bded848d8
EmilioSantillana <emiprec77@gmail.com>
2020-10-17 12:23:55 -0500

    Contador de vidas
    Agregue un contador de vidas

--- Elemental Legend/Assets/Scripts/LevelManager.cs
+++ Elemental Legend/Assets/Scripts/LevelManager.cs
@@ -8,20 +8,29 @@ public class LevelManager : MonoBehaviour
 {
     public static int puntuacion = 0;
     public string puntuacionString = "Puntuación = ";
-    public Text textScore;
+    public Text textScore, textVidas;
     public static LevelManager levelManager;
 
     private GameObject player;
 
     private void Awake()
     {
-        levelManager = this;
+        levelManager = this;      
+    }
 
+    private void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
+
+        if (textVidas != null)
+        {
+            textVidas.text = "x" + player.GetComponent<PlayerHealth>().vidas.ToString();
+        }
+
         if (textScore != null)
         {
             textScore.text = puntuacionString + puntuacion.ToString();
